c75483eba07d479c97b5dc96b59d600bfe836cb5
Hamander <1hamander@gmail.com>
2022-02-22 20:16:40 -0300

    working-with-numbers

---
+++ working-with-numbers/working-with-numbers.rb
@@ -0,0 +1,23 @@
+
+puts 10 + 10 ## Soma
+puts 10 - 5 ## Subtração
+puts 10 * 5 ## Mutiplicação
+puts 10 / 2 ## Divisão
+puts 2**3 ## Elevação
+puts 10 % 3 ## Resto da Divisão
+
+puts 3.0 ## Float number
+puts 3 ## Interger number
+
+favorite_number = -30.1
+
+puts "meu numbero favorito é #{favorite_number}"
+puts ("meu numbero favorito é " + favorite_number.to_s)
+
+puts favorite_number.abs()
+puts favorite_number.round()
+puts favorite_number.ceil()
+puts favorite_number.floor()
+puts Math.sqrt(36)
+
+
