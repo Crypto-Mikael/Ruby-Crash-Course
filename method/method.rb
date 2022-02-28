8380059f562ba6c50e039da4b2b608968ba37cd5
Hamander <1hamander@gmail.com>
2022-02-28 16:09:03 -0300

    build: method comprehension

---
+++ method/method.rb
@@ -0,0 +1,14 @@
+def sayhi(name="noName", age=-1)
+  puts "Olá #{name}, you are #{age}"
+end
+
+puts "Cima"
+sayhi("Miguel")
+puts "Baixo"
+
+def cube(num)
+  return num**num
+  5
+end
+
+puts cube(3)
