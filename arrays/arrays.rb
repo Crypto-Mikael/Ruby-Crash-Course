ebe4eaae359b7840bdcf74225860c084f525ec30
Hamander <1hamander@gmail.com>
2022-02-28 15:55:15 -0300

    build: array comprehension

---
+++ arrays/arrays.rb
@@ -0,0 +1,12 @@
+
+friends = Array.new
+
+friends[0] = "Andrey"
+friends[1] = "Gabriel"
+friends[2] = "Ilgner"
+
+
+puts friends.length()
+puts friends.include? "Gabriel"
+puts friends.reverse()
+puts friends.sort()
